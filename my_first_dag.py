75447ec23f991336391cae3494548f5e2e76f57e
sdengi <78135394+sdengi@users.noreply.github.com>
2021-05-30 22:41:01 +0530

    comiting files

---
+++ my_first_dag.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+import os
+
+print(os.getcwd())
+
+
+args = {'owner':'shankar'}
+
+dag = DAG(
+    dag_id = 'example_bash_operator',
+    default_args= args,
+    schedule_interval= daily,
+    start_date= today,
+    tags = ['example','eaxample2']
+    )
+
+run_this_first = DummyOperator(
+    task_id = "run_this_first",
+    dag = dag
+)
+
+run_this_second = BashOperator(
+    task_id = 'run_this_second',
+    bash_command = 'echo 1',
+    dag = dag
+)
+
+run_this_third = BashOperator(
+    task_id = 'run_this_third',
+    bash_command = 'pwd',
+    dag = dag
+)
+
+run_this_first >> run_this_second >> run_this_third
